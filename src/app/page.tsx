c9b087bbfee69c435db632a27720ad66c7457450
Irfan Rifani <92352067+Initfan@users.noreply.github.com>
2025-06-01 23:19:51 +0700

    Change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function WeatherApp() {
 			<div className="flex flex-col items-center min-h-screen p-4 ">
 				<div className="w-full max-w-lg mx-auto space-y-6">
 					<h1 className="text-3xl font-bold text-center my-4 text-white">
-						Aplikasi Perkiraan Cuaca
+						Aplikasi Prakiraan Cuaca
 					</h1>
 
 					<div className="bg-white rounded-lg shadow-lg p-6 space-y-4">
